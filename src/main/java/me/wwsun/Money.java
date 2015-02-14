958f94846fa3713c1dde1d4d93a34f0d65117064
Weiwei SUN <ww.sun@outlook.com>
2015-02-14 12:17:33 +0800

    t08 object factory

--- src/main/java/me/wwsun/Money.java
+++ src/main/java/me/wwsun/Money.java
@@ -3,8 +3,18 @@ package me.wwsun;
 /**
  * Created by Weiwei on 2015/2/14.
  */
-public class Money {
+public abstract class Money {
     protected int amount;
+
+    abstract Money times(int multiplier);
+
+    static Money dollar(int amount) {
+        return new Dollar(amount);
+    }
+
+    static Money franc(int amount) {
+        return new Franc(amount);
+    }
 
     @Override
     public boolean equals(Object obj) {
