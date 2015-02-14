19f3f2620e52b09ef48d27ca6b5f4e980faf1657
Weiwei SUN <ww.sun@outlook.com>
2015-02-14 14:52:00 +0800

    t11 delete testFrancMultiplication

--- src/main/java/me/wwsun/Dollar.java
+++ src/main/java/me/wwsun/Dollar.java
@@ -9,10 +9,4 @@ public class Dollar extends Money {
     Dollar(int amount, String currency) {
         super(amount, currency);
     }
-
-    @Override
-    String currency() {
-        return currency;
-    }
-
 }
--- src/test/java/me/wwsun/CurrencyTest.java
+++ src/test/java/me/wwsun/CurrencyTest.java
@@ -21,7 +21,8 @@ import junit.framework.TestSuite;
  * 06: Common equals
  * 08-10: Common times
  * 07: Compare Francs with Dollars
- * Todo: Currency?
+ * Currency?
+ * 11: delete testFrancMultiplication
  *
  */
 public class CurrencyTest extends TestCase {
@@ -49,18 +50,9 @@ public class CurrencyTest extends TestCase {
         assertEquals(Money.dollar(15), five.times(3));
     }
 
-    public void testFrancMultiplication() {
-        Money five = Money.franc(5);
-        assertEquals(Money.franc(10), five.times(2));
-        assertEquals(Money.franc(15), five.times(3));
-    }
-
     public void testEquality() {
         assertTrue(Money.dollar(5).equals(Money.dollar(5)));
         assertFalse(Money.dollar(5).equals(Money.dollar(6)));
-
-        assertTrue(Money.franc(5).equals(Money.franc(5)));
-        assertFalse(Money.franc(5).equals(Money.franc(6)));
 
         assertFalse(Money.franc(5).equals(Money.dollar(5)));
     }
